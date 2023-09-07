464a516d89b4f7b93624c2700c653e4028891fd5
heulerkretli <138309221+heulerkretli@users.noreply.github.com>
2023-09-07 15:22:14 -0300

    Add files via upload

---
+++ slugs/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+import requests
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+
+@app.route('/api/extract-slugs', methods=['GET'])
+def extract_slugs():
+    url = request.args.get('url')
+    
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    
+    slugs = []
+    
+    for link in soup.find_all('a'):
+        slug = link.get('href')
+        if slug:
+            slugs.append(slug)
+    
+    return jsonify({'slugs': slugs})
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ slugs/script.js
@@ -0,0 +1,29 @@
+document.getElementById('domain-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const domainInput = document.getElementById('domain');
+    const slugList = document.getElementById('slug-list');
+
+    const domain = domainInput.value;
+    
+    fetch(`/api/extract-slugs?url=${encodeURIComponent(domain)}`)
+        .then(response => response.json())
+        .then(data => {
+            slugList.innerHTML = '';
+
+            if (data.slugs && data.slugs.length > 0) {
+                data.slugs.forEach(slug => {
+                    const slugItem = document.createElement('p');
+                    slugItem.textContent = slug;
+                    slugList.appendChild(slugItem);
+                });
+            } else {
+                const noSlugsMessage = document.createElement('p');
+                noSlugsMessage.textContent = 'Nenhuma slug encontrada.';
+                slugList.appendChild(noSlugsMessage);
+            }
+        })
+        .catch(error => {
+            console.error('Erro ao extrair slugs:', error);
+        });
+});
