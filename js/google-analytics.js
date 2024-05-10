73fda4afc77a29fda6e51a9f112792af0245ad3c
heulerkretli <138309221+heulerkretli@users.noreply.github.com>
2024-05-10 19:52:20 -0300

    Add files via upload

---
+++ js/google-analytics.js
@@ -0,0 +1,27 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-205063874-1', {
+    'linker': {
+        'domains': ['clickbank.net']
+    }
+});
+
+<!-- End Global site tag (gtag.js) - Google Analytics -->
+
+
+<!-- Google Analytics -->
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-205063874-1', 'auto');
+
+<!-- End Google Analytics -->
+
+<!-- GA4 -->
+gtag('config', 'G-6JVRZ5YP70');
---
+++ js/tracking.js
@@ -0,0 +1,12 @@
+<!-- Taboola Pixel Code -->
+    window._tfa = window._tfa || [];
+    window._tfa.push({notify: 'event', name: 'page_view', id: 1588126});
+    !function (t, f, a, x) {
+    if (!document.getElementById(x)) {
+    t.async = 1;t.src = a;t.id=x;f.parentNode.insertBefore(t, f);
+}
+}(document.createElement('script'),
+    document.getElementsByTagName('script')[0],
+    '//cdn.taboola.com/libtrc/unip/1588126/tfa.js',
+    'tb_tfa_script');
+<!-- End of Taboola Pixel Code -->
